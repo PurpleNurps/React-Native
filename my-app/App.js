dfa67926fd57e645a9bbe9928714aa2e866810bf
PurpleNurps <seanlunt1991@gmail.com>
2024-02-15 20:40:03 +0000

    started attempt to publish

--- my-app/App.js
+++ my-app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yoooooooooooooooooooooooooo</Text>
+      <Text>Yoooo</Text>
       <StatusBar style="auto" />
     </View>
   );
